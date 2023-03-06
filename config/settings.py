34a68608f8a1acb6411755eba112e1abefb0239a
Navid Rahimzadeh <itsthenavid@gmail.com>
2023-03-06 16:09:55 +0330

    active static files support

--- config/settings.py
+++ config/settings.py
@@ -143,11 +143,11 @@ LOCALE_PATHS = [
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / "static/"
+# STATIC_ROOT = BASE_DIR / "static/"
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static/"
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static/"
+]
 
 MEDIA_URL = "/media/"
 
