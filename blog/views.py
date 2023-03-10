5cbfd27f03fe3460ecb8b00dacbc7f3a9f99cc24
Navid Rahimzadeh <itsthenavid@gmail.com>
2023-03-10 12:17:34 +0330

    write the base model of post list & detail view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+from .models import Post
+
 # Create your views here.
+
+
+def post_list_view(request):
+    post_list = Post.actives.all()
+
+    context = {
+        "posts": post_list,
+    }
+    return render(request, "blog/post_list.html", context)
+
+
+def post_detail_view(request, slug):
+    post = Post.actives.get(slug=slug)
+
+    context = {
+        "post": post,
+    }
+    return render(request, "blog/post_detail.html", context)
