085250b944042a6e8d2edf487e3f1ef6055f0781
Navid Rahimzadeh <itsthenavid@gmail.com>
2023-03-15 02:01:14 +0330

    include new views

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .views import PostListView, PostDetailView
+from .views import post_list_view, post_detail_view
 
 # Write URL Patterns here.
 
 app_name = "blog"
 
 urlpatterns = [
-    path("", PostListView.as_view(), name="post_list"),
-    path("<slug:slug>/", PostDetailView.as_view(), name="post_detail"),
+    path("", post_list_view, name="post_list"),
+    path("<slug:slug>/", post_detail_view, name="post_detail"),
 ]
